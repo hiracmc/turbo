e73a67a30d8dccc95cd0ca78a6501eca93e79a44
hiracmc <120374767+hiracmc@users.noreply.github.com>
2025-02-20 08:42:36 +0900

    test

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,22 @@
 
+class HelloWorld {
+    getInfo() {
+      return {
+        id: 'sapi',
+        name: ' ScriptAPI',
+        blocks: [
+          {
+            opcode: 'hello',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'import'
+          }
+        ]
+      };
+    }
+  
+    hello() {
+      return 'World!';
+    }
+  }
+  
+  Scratch.extensions.register(new HelloWorld())
